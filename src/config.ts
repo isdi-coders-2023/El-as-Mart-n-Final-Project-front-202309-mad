88131179122e5f3e31091bd7aaa5335647217253
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-06 18:55:08 +0100

    Change render succesful link to config file

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ console.log('LOCATION', location); // Temp
 export const serverUrl =
   location.hostname === 'localhost'
     ? 'http://localhost:3800'
-    : 'https://Elias-Martin-Final-Project-back-202309-mad.onrender.com'; // DEL SERVIDOR DE RENDER
+    : 'https://isdi-coders-2023-elias-martin-final.onrender.com'; // DEL SERVIDOR DE RENDER
 
 console.log(serverUrl); // Temp
