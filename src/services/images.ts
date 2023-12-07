4735f24ed23cd2d1acbb6eb74b633ab76f227a47
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-07 12:44:27 +0100

    Add users custom hook test

--- src/services/images.ts
+++ src/services/images.ts
@@ -1,5 +1,5 @@
 export const makeImageURL = (publicID: string, height: number) => {
   const urlBase = 'http://res.cloudinary.com/djz7c5bdp/image/upload';
-  const url = `${urlBase}/h_${height},c_fill,g_face,r_max/v1701292330/${publicID}`;
+  const url = `${urlBase}/h_${height}/${publicID}`;
   return url;
 };
