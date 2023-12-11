4eb3a1810ed848157fd21ce928f502a1beacfbe5
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-11 20:54:12 +0100

    Add details component content and scss style

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,33 +1,80 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import './details.scss';
+import { makeImageURL } from '../../services/images';
 
 export function Details() {
   const { currentClothingItem } = useSelector(
     (state: RootState) => state.clothesState
   );
+  const mobileBigClothingItemFrontImg =
+    currentClothingItem &&
+    currentClothingItem.clothingItemFrontImg &&
+    makeImageURL(currentClothingItem?.clothingItemFrontImg.publicId, 340);
+  const mobileSmallClothingItemFrontImg =
+    currentClothingItem &&
+    currentClothingItem.clothingItemFrontImg &&
+    makeImageURL(currentClothingItem?.clothingItemFrontImg.publicId, 140);
+  const mobileBigClothingItemBackImg =
+    currentClothingItem &&
+    currentClothingItem.clothingItemBackImg &&
+    makeImageURL(currentClothingItem?.clothingItemBackImg.publicId, 340);
+  const mobileSmallClothingItemBackImg =
+    currentClothingItem &&
+    currentClothingItem.clothingItemBackImg &&
+    makeImageURL(currentClothingItem?.clothingItemBackImg.publicId, 140);
+
   return (
     <div className="details-container">
       <div className="details-image-container">
-        <img
-          src={currentClothingItem?.clothingItemFrontImg.url}
-          alt={currentClothingItem?.name}
-        />
+        <div className="big-details-image-container">
+          <img
+            src={mobileBigClothingItemBackImg!}
+            alt={currentClothingItem?.name}
+            className="mobile-big-back-img"
+          />
+        </div>
+        <div className="small-details-images-container">
+          <img
+            src={mobileSmallClothingItemFrontImg!}
+            alt={currentClothingItem?.name}
+            className="mobile-small-front-img"
+          />
+          <img
+            src={mobileSmallClothingItemBackImg!}
+            alt={currentClothingItem?.name}
+            className="mobile-small-back-img"
+          />
+        </div>
       </div>
       <div className="details-info-container">
         <div className="details-name-container">
-          <p>{currentClothingItem?.name}</p>
+          <p>
+            {currentClothingItem?.name.toUpperCase()}
+            <span> ({currentClothingItem?.size})</span>
+          </p>
         </div>
-        {/* <p>
-          <span className="details-bold-text">Director:</span>{' '}
-          {currentFilm?.director}
-        </p>
-        <p>
-          <span className="details-bold-text">Era:</span> {currentFilm?.era}
-        </p>
-        <p>
-          <span className="details-bold-text">Año:</span> {currentFilm?.year}
-        </p> */}
+        <div className="details-price-container">
+          <p>{currentClothingItem?.price}</p>
+        </div>
+        <div className="details-size-container">
+          <p>
+            Talla de la prenda: <span>{currentClothingItem?.size}</span>
+          </p>
+        </div>
+        <div className="details-especific-sizes-container">
+          <p>
+            Largo: <span>{currentClothingItem?.clothingItemHeight}</span>
+          </p>
+          <p>
+            Ancho: <span>{currentClothingItem?.clothingItemWidth}</span>
+          </p>
+        </div>
+        <div className="details-tares-container">
+          <p>
+            Taras: <span>{currentClothingItem?.tares}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
