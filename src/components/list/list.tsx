b0c59f739265bc9a40fa8a2fb61e5502586f0202
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-12 19:22:08 +0100

    Fix a code smell

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -13,19 +13,17 @@ export function List() {
   }, [loadClothes]);
 
   return (
-    <>
-      <div className="list-container">
-        <RetroPhotos></RetroPhotos>
-        <div className="list-title-container">
-          <h2>LAST DROP</h2>
-        </div>
-        <Filter></Filter>
-        <ul className="clothes-list">
-          {clothes.map((item) => (
-            <Card key={item.id} clothingItem={item}></Card>
-          ))}
-        </ul>
+    <div className="list-container">
+      <RetroPhotos></RetroPhotos>
+      <div className="list-title-container">
+        <h2>LAST DROP</h2>
       </div>
-    </>
+      <Filter></Filter>
+      <ul className="clothes-list">
+        {clothes.map((item) => (
+          <Card key={item.id} clothingItem={item}></Card>
+        ))}
+      </ul>
+    </div>
   );
 }
