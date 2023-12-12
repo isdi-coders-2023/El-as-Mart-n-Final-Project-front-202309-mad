9d05e60fa3c72aa781f8ee57ea6988ec2debeedb
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-12 13:38:23 +0100

    Add filter component test

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -1,23 +1,23 @@
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { AdminPanel } from './admin.panel';
+import { Filter } from './filter';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { appStore } from '../../store/store';
 
-describe('Given AdminPanel component', () => {
+describe('Given Filter component', () => {
   describe('When we instantiate it', () => {
     beforeEach(() => {
       render(
         <BrowserRouter>
           <Provider store={appStore}>
-            <AdminPanel></AdminPanel>
+            <Filter></Filter>
           </Provider>
         </BrowserRouter>
       );
     });
     test('Then it should be in the document', () => {
-      const element = screen.getByRole('heading');
+      const element = screen.getByRole('combobox');
       expect(element).toBeInTheDocument();
     });
   });
