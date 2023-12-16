c3eae0e8f52078d1acd0434156b671672ee68797
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-16 19:58:24 +0100

    Add test to filter component

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -4,6 +4,14 @@ import { Filter } from './filter';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { appStore } from '../../store/store';
+import userEvent from '@testing-library/user-event';
+import { useClothes } from '../../hooks/clothes/use.clothes';
+
+jest.mock('../../hooks/clothes/use.clothes', () => ({
+  useClothes: jest.fn().mockReturnValue({
+    handleFilter: jest.fn(),
+  }),
+}));
 
 describe('Given Filter component', () => {
   describe('When we instantiate it', () => {
@@ -16,9 +24,11 @@ describe('Given Filter component', () => {
         </BrowserRouter>
       );
     });
-    test('Then it should be in the document', () => {
+    test('Then it should be in the document', async () => {
       const element = screen.getByRole('combobox');
       expect(element).toBeInTheDocument();
+      await userEvent.selectOptions(element, 'S');
+      expect(useClothes().handleFilter).toHaveBeenCalled();
     });
   });
 });
